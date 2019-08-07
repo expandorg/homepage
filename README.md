d20edae1a7d591bbedcd5f883df7fe6efbdce0bb
Slava Sobolev <sobolevslava@gmail.com>
2019-08-07 13:19:52 +0300

    gallery 1

---
+++ README.md
@@ -0,0 +1,1 @@
+Requester landing page
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,12 +5,13 @@ import styles from './Gallery.module.css';
 export default class Gallery extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			selected: 0
 		};
 	}
 
-	render() {
+	render(_, state) {
 		return (
 			<div className={styles.container}>
 				<div className={styles.header}>
