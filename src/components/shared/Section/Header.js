9e3bd972b7ced9c4910461cf554562e8379abd2a
Slava Sobolev <sobolevslava@gmail.com>
2019-08-09 10:23:10 +0300

    line

--- src/components/shared/Section/Header.js
+++ src/components/shared/Section/Header.js
@@ -1,15 +1,13 @@
 import { h } from 'preact';
 import cn from 'classnames';
 
-import Line from './Line';
-
 import styles from './Header.module.css';
 
 export default function Header({ color, title }) {
   return (
     <div className={cn(styles.header, styles[color])}>
       <div className={styles.title}>{title}</div>
-      <Line />
+      <div className={styles.line} />
     </div>
   );
 }
--- src/components/shared/Section/Line.js
+++
@@ -1,7 +0,0 @@
-import { h } from 'preact';
-
-import styles from './Line.module.css';
-
-export default function Line() {
-  return <div className={styles.container} />;
-}
