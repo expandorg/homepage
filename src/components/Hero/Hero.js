8ccf4fb27afc98bcf19cd93041848393ca5ae600
Slava Sobolev <sobolevslava@gmail.com>
2019-08-10 10:19:03 +0300

    tab gallery

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,28 +8,30 @@ export default function Hero() {
   return (
     <div className={styles.container}>
       <div className={styles.hero}>
-        <h1 className={styles.title}>
-          A new and better
-          <br />
-          micro task
-          <br />
-          marketplace.
-        </h1>
-        <h3 className={styles.subtitle}>
-          Easy to Get Started. High-Quality Worker Base.
-          <br />
-          Ethical Pricing for Workers & Requesters.
-        </h3>
-        <ActionLink
-          className={styles.signup}
-          href="https://requester.expand.org/signup"
-        >
-          Sign up
-        </ActionLink>
-        <div className={styles.secondary}>
-          <a className={styles.worker} href="https://expand.org">
-            Are you a worker?
-          </a>
+        <div className={styles.inner}>
+          <h1 className={styles.title}>
+            A new and better
+            <br />
+            micro task
+            <br />
+            marketplace.
+          </h1>
+          <h3 className={styles.subtitle}>
+            Easy to Get Started. High-Quality Worker Base.
+            <br />
+            Ethical Pricing for Workers & Requesters.
+          </h3>
+          <ActionLink
+            className={styles.signup}
+            href="https://requester.expand.org/signup"
+          >
+            Sign up
+          </ActionLink>
+          <div className={styles.secondary}>
+            <a className={styles.worker} href="https://expand.org">
+              Are you a worker?
+            </a>
+          </div>
         </div>
       </div>
     </div>
